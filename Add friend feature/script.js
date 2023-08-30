68831993b64e24e23ca21bc747657ec91a1844e3
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-08-30 15:42:10 +0530

    javascript  file add to the project

---
+++ Add friend feature/script.js
@@ -0,0 +1,20 @@
+let istatus = document.querySelector("h5");
+let btn = document.querySelector("#add");
+
+let check = 0;
+
+  btn.addEventListener("click", function () {
+    if (check == 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+    btn.innerHTML = "Remove Friend"
+    check = 1;
+    }
+    else{
+        istatus.innerHTML ="Stranger";
+        istatus.style.color = "red";
+        check = 0;
+        btn.innerHTML = "Add Friend"
+    }
+  });
+
