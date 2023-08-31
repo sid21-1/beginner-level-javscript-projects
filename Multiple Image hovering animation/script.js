ac34cf61805344299badb89736df225b54880b30
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-08-31 11:45:12 +0530

    beautifying the code

--- Multiple Image hovering animation/script.js
+++ Multiple Image hovering animation/script.js
@@ -1,43 +1,19 @@
 let box = document.querySelectorAll(".box");
 
-box.forEach(function(val){
+box.forEach(function (val) {
+  val.addEventListener("mouseenter", function () {
+    val.childNodes[3].style.opacity = 1;
+  });
 
-    
+  val.addEventListener("mouseleave", function () {
+    val.childNodes[3].style.opacity = 0;
+  });
 
-    val.addEventListener("mouseenter", function(){
-       val.childNodes[3].style.opacity = 1 ;
-    })
-
-    val.addEventListener("mouseleave", function(){
-        val.childNodes[3].style.opacity = 0 ;
-    })
-
-    val.addEventListener("mousemove", function(dets){
-        val.childNodes[3].style.left = dets.x+"px"
-        // val.childNodes[3].style.top= dets.y+"px"
-    })
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  val.addEventListener("mousemove", function (dets) {
+    val.childNodes[3].style.left = dets.x + "px";
+    // val.childNodes[3].style.top= dets.y+"px"
+  });
+});
 
 // let boxImage = document.querySelector("#box1 img");
 
