1d3ece8f7f2312d29f9f61940262b5d0f2da6bdb
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-09-01 19:28:47 +0530

    done with the scripting

--- Age calculator/script.js
+++ Age calculator/script.js
@@ -1,43 +1,47 @@
 let userInput = document.getElementById("date");
-userInput.max= new Date().toISOString().split('T')[0];
+userInput.max = new Date().toISOString().split("T")[0];
 
-function calculate(){
-    let InputDate = new Date(userInput.value)
+function calculate() {
+  let InputDate = new Date(userInput.value);
 
-    let userInputYear= InputDate.getFullYear();
-    let userInputMonth= InputDate.getMonth() + 1;
-    let userInputDate= InputDate.getDate();
+  let userInputYear = InputDate.getFullYear();
+  let userInputMonth = InputDate.getMonth() + 1;
+  let userInputDate = InputDate.getDate();
+  let result = document.getElementById("result");
 
-    let today= new Date();
+  let today = new Date();
 
-    let currentYear= today.getFullYear();
-    let currentMonth= today.getMonth() + 1;
-    let currentDay= today.getDate();
+  let currentYear = today.getFullYear();
+  let currentMonth = today.getMonth() + 1;
+  let currentDay = today.getDate();
 
-    let differenceInYears , differenceInMonths , differenceInDays;
+  let differenceInYears, differenceInMonths, differenceInDays;
 
-    // Here we are calculating the year difference 
-    differenceInYears = currentYear - userInputYear // 2023 -2000
+  // Here we are calculating the year difference
+  differenceInYears = currentYear - userInputYear; // 2023 -2000
 
+  // Here we are calculating the month difference
+  if (currentMonth >= userInputMonth) {
+    differenceInMonths = currentMonth - userInputMonth;
+  } else {
+    differenceInYears--;
+    differenceInMonths = 12 + currentMonth - userInputMonth;
+  }
+  if (currentDay >= userInputDate) {
+    differenceInDays = currentDay - userInputDate;
+  } else {
+    differenceInDays =
+      getDaysInMonths(userInputYear, userInputMonth) +
+      currentMonth -
+      userInputDate;
+  }
+  if (differenceInMonths < 0) {
+    differenceInMonths = 11;
+    differenceInYears--;
+  }
+  result.innerHTML = `You are ${differenceInYears} years, ${differenceInMonths} months and ${differenceInDays} days old `;
 
-    // Here we are calculating the month difference
-    if(currentMonth >= userInputMonth){
-        differenceInMonths = currentMonth - userInputMonth;
-    }
-    else{
-        differenceInYears--;
-        differenceInMonths = 12 + currentMonth - userInputMonth;
-    }
-    if(currentDay >= userInputDate){
-        differenceInDays = currentDay - userInputDate;
-    }
-    else{
-
-        differenceInDays = getDaysInMonths(userInputYear, userInputMonth) + currentMonth - userInputDate
-    }
-
-
-    function getDaysInMonths(year, month){
-        return new Date(year, month , 0).getDate();
-    }
+  function getDaysInMonths(year, month) {
+    return new Date(year, month, 0).getDate();
+  }
 }
