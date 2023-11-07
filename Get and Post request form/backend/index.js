5674bc85b98c47a5884f9121ae6ff624f7c03fd1
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-07 16:34:20 +0530

    sending information using post request form

--- Get and Post request form/backend/index.js
+++ Get and Post request form/backend/index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/register", (req, res)=>{
-    let {user, password}=req.query;
-    res.send(`standard GET response. Welcome ${user}`)
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.post("/register", (req, res)=>{
-    res.send("standard Post response")
-})
+app.get("/register", (req, res) => {
+  let { user, password } = req.query;
+  res.send(`standard GET response. Welcome ${user}`);
+});
 
-app.listen(port ,()=>{
-    console.log(`listening to port ${port}`);
-})
+app.post("/register", (req, res) => {
+  // res.send("standard Post response")
+  let { user, password } = req.body;
+  res.send(`standard Post response. Welcome ${user}`);
+});
+
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
