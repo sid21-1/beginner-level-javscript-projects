1324ad456bc27e1893eca88d6dd7225004413af1
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-08 16:05:58 +0530

    adding uuid fo unique id's

--- REST Class/index.js
+++ REST Class/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+// ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,17 +14,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
-    id: "1a",
+    id: uuidv4(),
     username: "Siddhanth Gupta",
     content: "I love coding",
   },
   {
-    id: "2b",
+    id: uuidv4(),
     username: "Vedanth Gupta",
     content: "I love playing cricket",
   },
   {
-    id: "3c",
+    id: uuidv4(),
     username: "Ravi Gupta",
     content: "I love eating",
   },
@@ -43,7 +45,8 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
   let { username, content } = req.body;
-  posts.push({ username, content });
+  let id = uuidv4();
+  posts.push({ id, username, content });
   res.redirect("/posts");
 });
 
